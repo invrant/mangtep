c8b8ac02b80986b34f7437e85332d6f13f60670b
Nataslamet <81040260+Nataslamet@users.noreply.github.com>
2021-04-25 14:53:12 +0700

    update cf.sh

--- cf.sh
+++ cf.sh
@@ -9,7 +9,7 @@ DOMAIN=nataserver.my.id
 sub=$(</dev/urandom tr -dc a-z0-9 | head -c4)
 SUB_DOMAIN=${sub}.nataserver.my.id
 CF_ID=nataslametsuberkah@gmail.com
-CF_KEY=d7646d2edd8f564e0da635c6b8b71c5fba0e2
+CF_KEY=0f8af5fcc449214023fa026d9ea776bcda643
 set -euo pipefail
 IP=$(wget -qO- icanhazip.com);
 echo "Updating DNS for ${SUB_DOMAIN}..."
